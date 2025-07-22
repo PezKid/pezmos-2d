5702035cdb34bf0cad1ab44e2c96eea193fad8e4
PezKid <pezmanmc@gmail.com>
2025-07-22 16:21:45 -0700

    Move previous tutorial to example.py. Add system to render 2d grid scalable by user input.

--- main.py
+++ main.py
@@ -1,34 +1,65 @@
 import pygame
+import math
+import numpy as np
 
-pygame.init()
+# colors
+BACKGROUND_COLOR = (255, 255, 255)
+GRID_COLOR = (100, 100, 100)
+AXES_COLOR = (0, 0, 0)
 
-SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+SCREEN_DIMS = SCREEN_WIDTH, SCREEN_HEIGHT = 800, 800
+GRID_SIZE = 20
+AXES_WIDTH = 3
+GRID_WIDTH = 1
+ORIGIN = (SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+screen = pygame.display.set_mode(SCREEN_DIMS)
 
-player = pygame.Rect((300, 250, 50, 50))
+def draw_axes(origin, color, width):
+  # get origin values
+  Ox, Oy = origin
+  
+  # horizontal
+  pygame.draw.line(screen, color, origin, (SCREEN_WIDTH, Oy), width=width)
+  pygame.draw.line(screen, color, origin, (0, Oy), width=width)
+  
+  # vertical
+  pygame.draw.line(screen, color, origin, (Ox, SCREEN_HEIGHT), width=width)
+  pygame.draw.line(screen, color, origin, (Ox, 0), width=width)
+  
+def draw_grid(origin, color, grid_size, width):
+  # get origin values
+  Ox, Oy = origin
+  
+  # horizontal
+  for yadj in np.arange(0, SCREEN_HEIGHT - Oy, grid_size):
+    posy, negy = Oy + yadj, Ox - yadj
+    pygame.draw.line(screen, color, (0, posy), (SCREEN_WIDTH, posy), width=width)
+    pygame.draw.line(screen, color, (0, negy), (SCREEN_WIDTH, negy), width=width)
+  
+  # vertical Q1, Q2
+  for xadj in np.arange(0, SCREEN_WIDTH - Ox, grid_size):
+    posx, negx = Ox + xadj, Ox - xadj
+    pygame.draw.line(screen, color, (posx, 0), (posx, SCREEN_WIDTH), width=width)
+    pygame.draw.line(screen, color, (negx, 0), (negx, SCREEN_WIDTH), width=width)
 
 running = True
 while (running):
   
-  screen.fill((0, 0, 0))
-  
-  pygame.draw.rect(screen, (255, 0, 0), player)
-  
   key = pygame.key.get_pressed()
-  if key[pygame.K_a]:
-    player.move_ip(-1, 0)
-  elif key[pygame.K_d]:
-    player.move_ip(1, 0)
-  if key[pygame.K_w]:
-    player.move_ip(0, -1)
-  elif key[pygame.K_s]:
-    player.move_ip(0, 1)
+  if (key[pygame.K_UP]):
+    GRID_SIZE = GRID_SIZE + 0.05
+  elif (key[pygame.K_DOWN]):
+    GRID_SIZE = GRID_SIZE - 0.05
+  
+  screen.fill(BACKGROUND_COLOR)
+  draw_grid(ORIGIN, GRID_COLOR, GRID_SIZE, GRID_WIDTH)
+  draw_axes(ORIGIN, AXES_COLOR, AXES_WIDTH)
   
   for event in pygame.event.get():
-    if event.type == pygame.QUIT:
+    if (event.type == pygame.QUIT):
       running = False
-      
-  pygame.display.update()
+  
+  pygame.display.flip()
 
 pygame.quit()
